6bb2080bdb78973f0a4b23c07df1a10ed9ab4c65
Ankur2903 <112171452+Ankur2903@users.noreply.github.com>
2022-08-27 20:31:56 +0530

    updated tut01

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,4 +1,8 @@
-def factorial(int x):
+def factorial(x):
+    product = 1
+    for i in range(1,x+1):
+        product = product*i
+    print("The factorial of " + str(x) + " is " + str(product))
     
 
 x=int(input("Enter the number whose factorial is to be found"))
