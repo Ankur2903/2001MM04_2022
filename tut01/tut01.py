4b8adcf3b168392cd73810a1f0e64d57d5e3a38d
Ankur2903 <112171452+Ankur2903@users.noreply.github.com>
2022-09-12 19:26:49 +0530

    updated overall count in tut01

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -53,6 +53,24 @@ def octact_identification(mod=5000):
                 octant.append(-3)
             else:
                 octant.append(-4)
+        
+        for j in range(0,n):
+            if octant[j] ==1:
+                total[0] = total[0] +1
+            elif octant[j] ==2:
+                total[2] = total[2] +1
+            elif octant[j] ==3:
+                total[4] = total[4] +1
+            elif octant[j] ==4:
+                total[6] = total[6] +1
+            elif octant[j] ==-1:
+                total[1] = total[1] +1
+            elif octant[j] ==-2:
+                total[3] = total[3] +1
+            elif octant[j] ==-3:
+                total[5] = total[5] +1
+            else:
+                total[7] = total[7] +1
 
     with open("octant_output.csv","w",newline = "") as file:
         writer = csv.writer(file)
@@ -60,15 +78,15 @@ def octact_identification(mod=5000):
             if i==0:
                 writer.writerow(["Time","U","V","W","U Avg","V Avg","W Avg","U'=U - U avg","V'=V - V avg","W'=W - w avg","Octant","","Octant ID","1","-1","2","-2","3","-3","4","-4"])
             elif i==1:
-                writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),str(avg_of_u),str(avg_of_v),str(avg_of_w),u_[i-1],v_[i-1],w_[i-1],str(octant[i-1]),"","Overall Count"])
+                writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),str(avg_of_u),str(avg_of_v),str(avg_of_w),u_[i-1],v_[i-1],w_[i-1],str(octant[i-1]),"","Overall Count",str(total[0]),str(total[1]),str(total[2]),str(total[3]),str(total[4]),str(total[5]),str(total[6]),str(total[7])])
             elif i==2:
                 writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),"","","",u_[i-1],v_[i-1],w_[i-1],str(octant[i-1]),"User Input","Mod "+ str(mod)])
             elif i==3:
-                 writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),"","","",u_[i-1],v_[i-1],w_[i-1],str(octant[i-1])])
+                 writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),"","","",u_[i-1],v_[i-1],w_[i-1],str(octant[i-1]),"",str((i-3)*mod)+"-"+str((i-2)*mod)])
             elif i>3 and i<=3+(n-1)/mod:
-                writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),"","","",u_[i-1],v_[i-1],w_[i-1],str(octant[i-1])])
+                writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),"","","",u_[i-1],v_[i-1],w_[i-1],str(octant[i-1]),"",str((i-3)*mod+1)+"-"+str((i-2)*mod)])
             else:
-                 writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),"","","",u_[i-1],v_[i-1],w_[i-1],str(octant[i-1])])
+                 writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),"","","",u_[i-1],v_[i-1],w_[i-1],str(octant[i-1]),"",""])
 
 mod=5000
 octact_identification(mod)
