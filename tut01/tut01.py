47fcabe92a12f608f0aa61238fd25ee0c63b8974
Ankur2903 <112171452+Ankur2903@users.noreply.github.com>
2022-09-12 18:38:46 +0530

    Updated averages in tut01

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,9 +1,53 @@
-def factorial(x):
-    product = 1
-    for i in range(1,x+1):
-        product = product*i
-    print("The factorial of " + str(x) + " is " + str(product))
-    
+import numpy as np
+def octact_identification(mod=5000):
+    time = []
+    u = []
+    v = []
+    w = []
+    u_ = []
+    v_ = []
+    w_ = []
+    octant = []
+    total = [0]*8
 
-x=int(input("Enter the number whose factorial is to be found"))
-factorial(x)
+    import csv
+    import os
+    os.system("cls")
+    with open("octant_input.csv","r") as file:
+        reader = csv.reader(file)
+        i=0
+        for x in reader:
+            if i==0:
+                time.append(x[0])
+                i = i+1
+            else:
+                time.append(x[0])
+                u.append(float(x[1]))
+                v.append(float(x[2]))
+                w.append(float(x[3]))
+                
+        n = len(u)
+        avg_of_u = np.mean(u)
+        avg_of_v = np.mean(v)
+        avg_of_w = np.mean(w)
+
+        
+
+    with open("octant_output.csv","w",newline = "") as file:
+        writer = csv.writer(file)
+        for i in range(n+1):
+            if i==0:
+                writer.writerow(["Time","U","V","W","U Avg","V Avg","W Avg","U'=U - U avg","V'=V - V avg","W'=W - w avg","Octant","","Octant ID","1","-1","2","-2","3","-3","4","-4"])
+            elif i==1:
+                writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1]),str(avg_of_u),str(avg_of_v),str(avg_of_w)])
+            elif i==2:
+                writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1])])
+            elif i==3:
+                 writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1])])
+            elif i>3 and i<=3+(n-1)/mod:
+                writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1])])
+            else:
+                 writer.writerow([time[i],str(u[i-1]),str(v[i-1]),str(w[i-1])])
+
+mod=5000
+octact_identification(mod)
