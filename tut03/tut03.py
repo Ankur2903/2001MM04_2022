479ad16e7fd7ee6d3660a852e57fd192b2c16aea
Ankur2903 <112171452+Ankur2903@users.noreply.github.com>
2022-09-29 15:21:05 +0530

    updated tut03

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -14,6 +14,7 @@ def octant_longest_subsequence_count():
         v_ = []#v - avg_of_v
         w_ = []#w - avg_of_w
         octant = []#store octant value
+        octants = ["+1","-1","+2","-2","3","-3","+4","-4"]
 
         for i in range(2, n + 1):
             time.append(sheet.cell(row=i, column=1).value)#append time value in time list
@@ -31,34 +32,36 @@ def octant_longest_subsequence_count():
             w_.append(w[i]-avg_of_w)
 
         for i in range(n-1):#for identfy octant
-                if u_[i]>=0 and v_[i]>=0 and w_[i]>0:
-                    octant.append(+1)
-                elif u_[i]<0 and v_[i]>=0 and w_[i]>0:
-                    octant.append(+2)
-                elif u_[i]<0 and v_[i]<0 and w_[i]>=0:
-                    octant.append(+3)
-                elif u_[i]>=0 and v_[i]<0 and w_[i]>=0:
-                    octant.append(+4)
-                elif u_[i]>=0 and v_[i]>=0 and w_[i]<=0:
-                    octant.append(-1)
-                elif u_[i]<0 and v_[i]>=0 and w_[i]<=0:
-                    octant.append(-2)
-                elif u_[i]<0 and v_[i]<0 and w_[i]<0:
-                    octant.append(-3)
-                else:
-                    octant.append(-4)
-
+            if u_[i]>=0 and v_[i]>=0 and w_[i]>0:
+                octant.append(+1)
+            elif u_[i]<0 and v_[i]>=0 and w_[i]>0:
+                octant.append(+2)
+            elif u_[i]<0 and v_[i]<0 and w_[i]>=0:
+                octant.append(+3)
+            elif u_[i]>=0 and v_[i]<0 and w_[i]>=0:
+                octant.append(+4)
+            elif u_[i]>=0 and v_[i]>=0 and w_[i]<=0:
+                octant.append(-1)
+            elif u_[i]<0 and v_[i]>=0 and w_[i]<=0:
+                octant.append(-2)
+            elif u_[i]<0 and v_[i]<0 and w_[i]<0:
+                octant.append(-3)
+            else:
+                octant.append(-4)
+        
         from openpyxl import Workbook
         book = Workbook()
         sheet = book.active
 
         rows = [
-            ["Time","U","V","W","U Avg","V Avg","W Avg","U'=U - U avg","V'=V - V avg","W'=W - w avg","Ocatant"]
+            ["Time","U","V","W","U Avg","V Avg","W Avg","U'=U - U avg","V'=V - V avg","W'=W - w avg","Ocatant","","Count","Longest Subsquence Length","Count"]
         ]#made 2d list
 
         for i in range(n-1):
             if i==0:#append 2nd line in rows
                 rows.append([time[i],u[i],v[i],w[i],avg_of_u,avg_of_v,avg_of_w,u_[i],v_[i],w_[i],octant[i]])
+            elif i<9:
+                rows.append([time[i],u[i],v[i],w[i],avg_of_u,avg_of_v,avg_of_w,u_[i],v_[i],w_[i],octant[i],"",octants[i-1]])
             else:
                 rows.append([time[i],u[i],v[i],w[i]," "," "," ",u_[i],v_[i],w_[i],octant[i]])
     
