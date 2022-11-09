15f1264b4029030bd491c0a315869bfec69aa690
Ankur2903 <112171452+Ankur2903@users.noreply.github.com>
2022-11-10 00:43:50 +0530

    updated tut06

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -2,10 +2,12 @@ import csv
 import os
 from tkinter import N
 os.system("cls")
-
-from datetime import datetime
-start_time = datetime.now()
 import numpy as np
+import calendar
+import datetime
+
+#from datetime import datetime
+#start_time = datetime.now()
 
 def attendance_report():
     rollNo = []
@@ -28,51 +30,6 @@ def attendance_report():
             attendance.append(x[1])
             m = m+1
 
-    list_class =["01/08","04/08","08/08","22/08","25/08","29/08","01/09","05/09","08/09","12/09"]
-    dict_rollno ={}
-    dict_attendance_count_actual = {}
-    dict_attendance_count_fake = {}
-    for i in range(1,n):
-        dict_rollno[rollNo[i]]=0
-        dict_attendance_count_actual[rollNo[i]]=0
-        dict_attendance_count_fake[rollNo[i]]=0
-    Total_count_of_attendance_on_that_day = ["Total_count_of_attendance_on_that_day"]
-    timestamp_1 = ["Timestamp"]
-    rollNo_1 = ["Rollno"]
-    name_1 = ["name"]
-    if timestamp[1][0:5] in list_class:
-        if timestamp[1][11:13]=="14":
-            dict_rollno[attendance[1][0:8]]+=1
-            dict_attendance_count_actual[attendance[1][0:8]]+=1
-        else:
-            dict_attendance_count_fake[attendance[1][0:8]]+=1
-    x = 1
-    for i in range(2,m):
-        if i!=m-1 and (attendance[i]=='' or attendance[i][0:8] not in dict_rollno.keys() or timestamp[i][0:5] not in list_class):
-            continue
-        if timestamp[i][0:10]==timestamp[x][0:10] and i!=m-1:
-            if timestamp[i][11:13]=="14":
-                dict_rollno[attendance[i][0:8]]+=1
-                if dict_rollno[attendance[i][0:8]]==1:
-                    dict_attendance_count_actual[attendance[i][0:8]]+=1
-            else:
-                dict_attendance_count_fake[attendance[i][0:8]]+=1
-        else:
-            for j in range(1,n):
-                if dict_rollno[rollNo[j]]>1:
-                    Total_count_of_attendance_on_that_day.append(dict_rollno[rollNo[j]])
-                    timestamp_1.append(timestamp[x][0:10])
-                    rollNo_1.append(rollNo[j])
-                    name_1.append(name[j])
-            for k in range(1,n):
-                dict_rollno[rollNo[k]]=0
-            if timestamp[i][0:5] in list_class and timestamp[i][11:13]=="14":
-                dict_rollno[attendance[i][0:8]]+=1
-                dict_attendance_count_actual[attendance[i][0:8]]+=1
-            else:
-                dict_attendance_count_fake[attendance[i][0:8]]+=1
-            x = i
-
     if os.path.exists("output"):
         for f in os.listdir("output"):
             os.remove(os.path.join("output",f))
@@ -82,26 +39,73 @@ def attendance_report():
     os.mkdir(curr.replace('\\','/')+"/output/")
     os.chdir("output")
 
-    with open("attendanc_report_consolidated.csv","w",newline ="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Roll","Name","total_lecture_taken","attendance_count_actual","attendance_count_fake","attendance_count_absent","Percentage (attendance_count_actual/total_lecture_taken) 2 digit decimal "])
-    with open("attendanc_report_duplicate.csv","w",newline ="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Timestamp","Roll","Name","Total count of attendance on that day"])
+    def days(date):
+        day = datetime.datetime.strptime(date,'%d-%m-%Y').weekday()
+        return (calendar.day_name[day])
+
+    list_date = []
+    for i in range(1,m):
+        if timestamp[i][0:10] not in list_date and (days(timestamp[i][0:10])=='Monday' or days(timestamp[i][0:10])=='Thursday'):
+            list_date.append(timestamp[i][0:10])
+    
+    total_att = []
+    real_att = []
+    dupl_att = []
+    for i in range(n-1):
+        total_att.append([0]*len(list_date))
+        real_att.append([0]*len(list_date))
+        dupl_att.append([0]*len(list_date))
+    
+    for i in range(1,m):
+        if timestamp[i][0:10] in list_date:
+            a = list_date.index(timestamp[i][0:10])
+            b = rollNo.index(attendance[i][0:8])
+            total_att[b-1][a]+=1
+            if timestamp[i][11:13]=='14' or timestamp[i][11:] =='15:00:00':
+                real_att[b-1][a]=1
+                dupl_att[b-1][a]+=1
+    
+
+
     for i in range(1,n):
-        with open(rollNo[i]+".csv","w",newline ="") as file:
-            writer = csv.writer(file)
-            writer.writerow(["Roll","Name","total_lecture_taken","attendance_count_actual","attendance_count_fake","attendance_count_absent","Percentage (attendance_count_actual/total_lecture_taken) 2 digit decimal "])
-            writer.writerow([rollNo[i],name[i],"10",dict_attendance_count_actual[rollNo[i]],dict_attendance_count_fake[rollNo[i]],10-dict_attendance_count_actual[rollNo[i]],np.round_((dict_attendance_count_actual[rollNo[i]]/10)*100,2)])
-        with open("attendanc_report_consolidated.csv","a",newline ="") as file:
-            writer = csv.writer(file)
-            writer.writerow([rollNo[i],name[i],"10",dict_attendance_count_actual[rollNo[i]],dict_attendance_count_fake[rollNo[i]],10-dict_attendance_count_actual[rollNo[i]],np.round_((dict_attendance_count_actual[rollNo[i]]/10)*100,2)])
-        if i<len(name_1):
-            with open("attendanc_report_duplicate.csv","a",newline ="") as file:
-                writer = csv.writer(file)
-                writer.writerow([timestamp_1[i],rollNo_1[i],name_1[i],Total_count_of_attendance_on_that_day[i]])
-   
+        from openpyxl import Workbook
+        book = Workbook()
+        sheet = book.active
+        rows = [["Date","Roll","Name","Total Attendance Count","Real","Duplicate","Invalid","Absent"]]
+        rows.append(["",rollNo[i],name[i]])
+        for j in range(len(list_date)):
+            rows.append([list_date[j],"","",total_att[i-1][j],real_att[i-1][j],dupl_att[i-1][j]-real_att[i-1][j],total_att[i-1][j]-dupl_att[i-1][j],1-real_att[i-1][j]])
+        for row in rows:
+            sheet.append(row)
+        book.save(rollNo[i] + '.xlsx')
+
+    from openpyxl import Workbook
+    book = Workbook()
+    sheet = book.active
+    rows1 = []
+    rows1.append(["Roll","Name"])
+    for r in list_date:
+        rows1[0].append(r)
+    rows1[0].append("Actual Lecture Taken")
+    rows1[0].append("Total Real")
+    rows1[0].append("% Attendance")
+    for i in range(1,n):
+        rows1.append([rollNo[i],name[i]])
+        for j in range(len(list_date)):
+            if real_att[i-1][j]==1:
+                rows1[i].append("P")
+            else:
+                rows1[i].append("A")
+        rows1[i].append(np.sum(len(list_date)))       
+        rows1[i].append(np.sum(real_att[i-1]))
+        rows1[i].append(np.round_((np.sum(real_att[i-1])/np.sum(len(list_date)))*100,2))
+    for row in rows1:
+        sheet.append(row)
+    book.save('attendance_report_consolidated.xlsx')
+            
+
+
 attendance_report()
 print("*****************************************************     run successfully     ******************************************************")
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+#end_time = datetime.now()
+#print('Duration of Program Execution: {}'.format(end_time - start_time))
